547dd8758fe63d14f92f4f072c340f862c78f470
miiriiam_gontier@hotmail.com <mugrosa1995>
2021-04-10 13:18:14 -0500

    Ejercicios Sesión 2

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Laptop',
+        image: 'black_lenovo.jpg',
+        url: 'https://intercompras.com/p/laptop-lenovo-ideapad-14ast-amd-a4-4gb-500gb-windows-home-159811',
+        inStock: true,
+        inventario: 100
+    }
+})
